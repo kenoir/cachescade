cae8f25cd90a7d485290eaa03005b7773f24cc8f
Integralist <mark.mcdx@gmail.com>
2014-02-28 10:15:35 +0000

    Use proper naming convention for instance/class methods

--- spec/cachescade_spec.rb
+++ spec/cachescade_spec.rb
@@ -30,7 +30,7 @@ describe Cachescade do
     let(:foo) { 'foo value' }
     let(:bar) { 'bar value' }
 
-    describe 'fetch(id)' do
+    describe '#fetch(id)' do
       context 'found in first cache layer' do
         it 'returns correct value' do
           Cachescade::Drivers::InMemory
@@ -54,7 +54,7 @@ describe Cachescade do
       end
     end
 
-    describe 'write(id, data)' do
+    describe '#write(id, data)' do
       it 'writes data to all layers' do
         valid_options.write('foo', foo).join # we use 'join' for purpose of the tests
 
